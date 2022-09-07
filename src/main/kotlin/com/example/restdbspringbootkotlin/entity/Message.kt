93b013436e884cd9717bc9ed3b083b3bd4a9f915
Jaewook <eari100@naver.com>
2022-09-07 22:17:17 +0900

    update: Message.id pk 선언

--- src/main/kotlin/com/example/restdbspringbootkotlin/entity/Message.kt
+++ src/main/kotlin/com/example/restdbspringbootkotlin/entity/Message.kt
@@ -1,6 +1,7 @@
 package com.example.restdbspringbootkotlin.entity
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("messages")
-data class Message(val id: String?, val text: String)
+data class Message(@Id val id: String?, val text: String)
